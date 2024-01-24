fad6b6737677e181a8288414a5f90ef92e15d512
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 23:50:53 +0200

    edited on Wed Jan 24 2024 21:50:39 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Products":{"name":"products","fields":{"Title":"Title","Description":"Description","Price":"Price","Category":"Category","Id":"Id"}},"Categories":{"name":"categories","fields":{"Name":"Name","Description":"Description","Id":"Id"}},"Users":{"name":"users","fields":{"Name":"Name","Email":"Email","Password":"Password","Id":"Id"}},"Orders":{"name":"orders","fields":{"Userid":"Userid","Productid":"Productid","Quantity":"Quantity","Totalprice":"Totalprice","Id":"Id"}},"Payments":{"name":"payments","fields":{"Orderid":"Orderid","Transactionid":"Transactionid","Status":"Status","Id":"Id"}}}};
+    const enResources = { resources: {"Product":{"name":"product","fields":{"Name":"Name","Description":"Description","Price":"Price","Manufacturer":"Manufacturer","Id":"Id"}},"User":{"name":"user","fields":{"Username":"Username","Email":"Email","Phonenumber":"Phonenumber","Shippingaddress":"Shippingaddress","Billingaddress":"Billingaddress","Orderhistory":"Orderhistory","Id":"Id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
