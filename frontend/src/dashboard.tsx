a68d286616d2aef413ab2efd560f3c20a4090bc5
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 23:56:42 +0200

    edited on Wed Jan 24 2024 21:56:06 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "niko" />
-      <CardContent> Ecommerce website selling VR related technology < /CardContent>
+      <CardHeader title= "apoelsite" />
+      <CardContent> A fan app for the sports club APOEL FC, featuring entities such as Players, Matches, and News < /CardContent>
       </Card>
     );
